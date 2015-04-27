5633ef70172bf6cb759fa22c463e46f08689dbd6
cassdotcom <cassdotcom@users.noreply.github.com>
2015-04-27 18:10:29 +0100

    Update CBM_TOP_LEVEL.ps1
    reformatted

--- CBM_TOP_LEVEL.ps1
+++ CBM_TOP_LEVEL.ps1
@@ -1,19 +1,20 @@
-﻿###########################################################
+###########################################################
 # .FILE		: CBM_Top_Level
 # .AUTHOR  	: A. Cassidy 
 # .DATE    	: 2015-04-10 
 # .EDIT    	: 
 # .FILE_ID	: PSCBM001
 # .COMMENT 	: Top Level CBM Script
-# .INPUT	: 
+# .INPUT	: debug_switch: cause no execution
 # .OUTPUT	: Valid & Not-Valid Model Paths
 #			  	
 #           
-# .VERSION : 0.1
+# .VERSION : 0.2
 ###########################################################
 # 
 # .CHANGELOG
-# Version 1.0: 2015-04-10 First version
+# Version 0.2: 2015-04-26 Reformatted
+# Version 0.1: 2015-04-10 First version
 # 
 # .INSTRUCTIONS FOR USE
 # Function declarations are at the end of the file.
@@ -21,94 +22,135 @@
 #
 ###########################################################
 
+# Inputs to script
+Param (
+    [Parameter(Position=0, mandatory=$false)]
+    [Alias("debug_switch")]
+    [System.String]
+    $dummy_run)
+
+#----------------------------------------------------------
+# STATIC VARIABLES
+#----------------------------------------------------------
+# 
+$LOCAL_TEST = "\\scotia.sgngroup.net\dfs\shared\Syn4.2.3\Scotland Networks\" 
+
 process
 {
+    
 #----------------------------------------------------------
 # USER INTERFACE
 #----------------------------------------------------------
 $gui_title = "NETWORK PLANNING CBM 14-15 CREATION SCRIPT"
 modify_gui -title $gui_title
 
-#----------------------------------------------------------
-# WRITE SCRIPT HEADER
-#----------------------------------------------------------
-# Write script header
-$scipt_title = "CBM CREATION SCRIPT v1.0"
-$cbm_user = $env:USERNAME
-$cbm_date = (Get-Date).ToString("yyyy-MM-dd @ hh:mm:ss")
-write_header -user $cbm_user -date $cbm_date -title $scipt_title
 
-#----------------------------------------------------------
-# BEGIN TESTING PATHS
-#----------------------------------------------------------
-$msg = "1. Test Model Paths"
-write_line -text $msg
+    #----------------------------------------------------------
+    # WRITE SCRIPT HEADER
+    #----------------------------------------------------------
+    # Write script header
+    $scipt_title = "CBM CREATION SCRIPT v1.0"
+    $cbm_user = $env:USERNAME
+    $cbm_date = (Get-Date).ToString("yyyy-MM-dd @ hh:mm:ss")
+    write_header -user $cbm_user -date $cbm_date -title $scipt_title
 
-Try
-{	# FileNotFound Exception will not cause PShell failure so explicitly state
-    $ErrorActionPreference = "Stop"
-    $cbm_model_list = Get-Content $MODEL_LIST
-    $valid_list,$not_valid_list,$n_valid,$n_invalid = test_model_paths -model_list $cbm_model_list
-    Start-Sleep 2
-    $msg = "   Valid: $($n_valid)"
+
+    #----------------------------------------------------------
+    # LOAD MODEL DB
+    #----------------------------------------------------------
+    $msg = "Load model DB"
     write_line -text $msg
-    $msg = "   Not Valid: $($n_invalid)"
-    write_line -text $msg
-    $msg = "   Writing result to file"
-    write_line -text $msg
-    $valid_list | Out-File $VALIDATED_LIST
-    $not_valid_list | Out-File $INVALIDATED_LIST
-}
-Catch
-{
-    $msg = "WARNING: UNABLE TO LOCATE MODEL LIST"
-    write_line -text $msg -siren True
-}
+
+    Try 
+    {
+	    $cbm_db = import-csv $CONSTANT_DATA.MODEL_LIST
+    }
+    Catch
+    {
+        $msg = "TERMINATE: COULD NOT IMPORT MODEL DATA"
+        write_line -text $msg
+        break
+    }
 
 
-#----------------------------------------------------------
-# TEST CBM_FOLDERS
-#----------------------------------------------------------
-$msg = "2. Test CBM Folders exist"
-write_new_section -text $msg
+    #----------------------------------------------------------
+    # BEGIN TESTING PATHS
+    #----------------------------------------------------------
 
-}#end process
+
 
 
 
 
 begin 
 {
+# functionality test
+if ( $dummy_run )
+{
+    Write-Host $dummy_run
+}
+else
+{
 
-#----------------------------------------------------------
-# STATIC VARIABLES
-#----------------------------------------------------------
-$global:USERPATH = "C:\Users\ac00418\"
-$MODEL_LIST = "\settings\cbm_model_list.txt"
+# Create data structure for reporting
+# Find if script is run off-line or network
+$CBM_SETTINGS = New-Object psobject
+$CBM_SETTINGS = get_cbm_settings -location $LOCAL_TEST
 
-#----------------------------------------------------------
-# REPORTING VARIABLES
-#----------------------------------------------------------
-$CBM_SETTINGS_FILE = "\settings\cmb_settings.ini"
-$CBM_LOG = "\logs\cbm_creation_log.txt"
-$CBM_OUTPUT = "\logs\cbm_models_output.csv"
-$VALIDATED_LIST = "\cbm_validated_list.txt"
-$INVALIDATED_LIST = "\cbm_invalidated_list.txt"
+function get_cbm_settings
+{
+    Param (
+        [Parameter(Position=0, mandatory=$true)]
+	    [Alias("location")]
+	    [System.String]
+	    $run_environment)
+
+$CONSTANT_DATA = New-Object psobject
+if (Test-Path $run_environment)
+{ # Can access network drives
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name USERPATH -Value "C:\Users\ac00418\"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name MODEL_LIST -Value "\settings\cbm_model_list.txt"
+
+    #----------------------------------------------------------
+    # REPORTING VARIABLES
+    #----------------------------------------------------------
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_SETTINGS_FILE -Value "\settings\cmb_settings.ini"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_LOG -Value "\logs\cbm_creation_log.txt"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_OUTPUT -Value "\logs\cbm_models_output.csv"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name VALIDATED_LIST -Value "\cbm_validated_list.txt"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name INVALIDATED_LIST -Value "\cbm_invalidated_list.txt"
+}
+else
+{
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name USERPATH -Value "C:\Users\ac00418\"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name MODEL_LIST -Value "C:\Users\ac00418\Desktop\synergee_scripts\CBM\model_data\sgn_network_models.csv"
+
+    #----------------------------------------------------------
+    # REPORTING VARIABLES
+    #----------------------------------------------------------
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_SETTINGS_FILE -Value "\settings\cmb_settings.ini"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_LOG -Value "\logs\cbm_creation_log.txt"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name CBM_OUTPUT -Value "\logs\cbm_models_output.csv"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name VALIDATED_LIST -Value "\cbm_validated_list.txt"
+    $CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name INVALIDATED_LIST -Value "\cbm_invalidated_list.txt"
+}
 
 #----------------------------------------------------------
 # GUI VARIABLES
 #----------------------------------------------------------
-$width_of_console = 92
-$gui_height = 50
-$gui_width = 125
-$dash = "-"
-$double_dash = "$($dash)$($dash)"
-$indent = 5
-$scr_buffer_blankline = (" " * $width_of_console+2)
-$scr_buffer_box = "  " +("$($dash)" * $width_of_console)
-$scr_buffer_box_nil = "  " + $double_dash + (" " * ($width_of_console - 4)) + $double_dash
-$scr_buffer_indent = "  " + $double_dash + "  "
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name width_of_console -Value 92
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name gui_height -Value 50
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name gui_width -Value 125
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name dash -Value "-"
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name double_dash -Value "$($CONSTANT_DATA.dash)$($CONSTANT_DATA.dash)"
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name indent -Value 5
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name scr_buffer_blankline -Value (" " * $CONSTANT_DATA.width_of_console+2)
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name scr_buffer_box -Value ("  " +("$($CONSTANT_DATA.dash)" * $CONSTANT_DATA.width_of_console))
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name scr_buffer_box_nil -Value ("  " + $CONSTANT_DATA.double_dash + (" " * ($CONSTANT_DATA.width_of_console - 4)) + $CONSTANT_DATA.double_dash)
+$CONSTANT_DATA | Add-Member -MemberType NoteProperty -Name scr_buffer_indent -Value ("  " + $CONSTANT_DATA.double_dash + "  ")
 
+return $CONSTANT_DATA
+}#end get_cbm_settings
 
 
 #----------------------------------------------------------
@@ -120,7 +162,15 @@ function modify_gui
         [Parameter(Position=0, mandatory=$true)]
 	    [Alias("title")]
 	    [System.String]
-	    $gui_title)
+	    $gui_title,
+        [Parameter(Position=1, mandatory=$false)]
+	    [Alias("height")]
+	    [System.String]
+	    $new_height = $CBM_SETTINGS.gui_height,
+        [Parameter(Position=2, mandatory=$false)]
+	    [Alias("width")]
+	    [System.String]
+	    $new_width = $CBM_SETTINGS.gui_width)
     
     Try
     {
@@ -136,8 +186,8 @@ function modify_gui
 
         # Resize window
         $newsize = $ui_console.windowsize
-        $newsize.height = $gui_height
-        $newsize.width = $gui_width
+        $newsize.height = $new_height
+        $newsize.width = $new_width
         $ui_console.windowsize = $newsize
         cls
     }
@@ -148,32 +198,6 @@ function modify_gui
 
 }#end modify_gui
 
-
-#----------------------------------------------------------
-# .function_SET-LOADING
-#----------------------------------------------------------
-function set-loading
-{
-    param (
-        [Parameter(Position=0, mandatory=$true)]
-	    [Alias("msg")]
-	    [System.String]
-	    $msg_out)
-
-    Write-Host $msg_out -NoNewline
-
-    $scr_buffer = @("")
-
-    for($i=0;$i -lt 30; $i++)
-    {
-        $scr_buffer = $scr_buffer + "."
-        Write-Host $scr_buffer -NoNewline
-        Start-Sleep -milliseconds 50
-    }
-
-    Write-Host "`tDONE"
-    Start-Sleep 1
-}#end set-loading
 
 
 
@@ -186,11 +210,24 @@ function complete_line
         [Parameter(Position=0, mandatory=$true)]
 	    [Alias("col")]
 	    [System.int32]
-	    $column)
+	    $column,
+        [Parameter(Position=1, mandatory=$false)]
+	    [Alias("alarm")]
+	    [switch]
+        [$alarm])
     
-    $lineRemaining = $width_of_console - $column
-    Write-Host -NoNewLine (" " * $lineRemaining)
+    $lineRemaining = $CBM_SETTINGS.width_of_console - $column
+    if ($alarm)
+    {
+        Write-Host -NoNewLine (" " * $lineRemaining) -ForegroundColor Yellow -BackgroundColor Black
+    }
+    else
+    {
+        Write-Host -NoNewLine (" " * $lineRemaining)
+    }
+        
 }#end complete_line
+
 
 
 
@@ -261,8 +298,6 @@ function write_header
 
 
 
-
-
 #----------------------------------------------------------
 # .function_WRITE_LINE
 #----------------------------------------------------------
@@ -284,7 +319,7 @@ function write_line
     if ($alarm)
     {
         Write-Host $text -ForegroundColor Yellow -BackgroundColor Black -NoNewline 
-	    complete_line $column
+	    complete_line $column -alarm
 	    Write-Host $double_dash -ForegroundColor Yellow -BackgroundColor Black
     }
     else
@@ -295,8 +330,6 @@ function write_line
     }
 
 }#end write_line
-
-
 
 
 
@@ -332,14 +365,17 @@ function test_model_paths
 {
     Param (
         [Parameter(Position=0, mandatory=$true)]
-	    [Alias("model_list")]
-	    [System.string]
-	    $model_list)
+        [Alias("model_list")]
+        [System.string]
+        $model_db)
     
     $valid_list = @()
     $not_valid_list = @()
 
-    $i = 0
+    $model_list = $model_db.FY1_PATH
+
+    # Assigning i will give ID, allowing models to be referenced later on.
+    $i = 1
     foreach ($m in $model_list)
     {
         if ( Test-Path $m)
@@ -360,18 +396,28 @@ function test_model_paths
     return $valid_list,$not_valid_list,$n_valid,$n_invalid
 }# end test_model_paths
 
+}#end else
+
+}#end begin
 
 
 
-
-
-
-} #end begin
 
 end
 {
-    # end
+    # return console to normal    
+    $ui_console = (Get-Host).UI.RawUI
+    $old_bc = $ui_console.BackgroundColor
+    $old_fc = $ui_console.ForegroundColor
+    $old_title = $ui_console.WindowTitle
+
+    # Change window to signal we're now in powershell
+    $ui_console.BackgroundColor = "darkblue"
+    $ui_console.ForegroundColor = "white"
+
+    # title 
+    $gui_title = "Powershell"
+    $ui_console.WindowTitle = $gui_title
+    cls
     Write-host "  Powershell script finished"
-} #end end
-
-
+}#end end
