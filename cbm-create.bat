0f91c822ed3fbcd14f02a478852ada19e753ce82
cassdotcom <cassdotcom@users.noreply.github.com>
2015-05-18 15:37:18 +0100

    Update cbm-create.bat
    version 1

--- cbm-create.bat
+++ cbm-create.bat
@@ -3,12 +3,11 @@ echo --
 echo --  
 echo -- NETWORK PLANNING CBM CREATION SCRIPT 2014-2015
 echo -- ----------------------------------------------
-@powershell -command "copy-item -Path 'S:\TEST AREA\ac00418\CBM\rev_i\cbm-create.ps1' -destination cbm-create.ps1"
-
+REM @powershell -command "copy-item -Path 'S:\TEST AREA\ac00418\CBM\rev_i\cbm-create.ps1' -destination cbm-create.ps1"
 echo --                                             
 echo -- BEGIN POWERSHELL
 rem powershell -ExecutionPolicy Bypass -noexit .\import_CBM_modules.ps1
 @powershell -ExecutionPolicy Bypass .\cbm-create.ps1
 rem @powershell -ExecutionPolicy Bypass .\CBM_TOP_LEVEL.ps1 $true
-@powershell -command "Remove-Item cbm-create.ps1"
+REM @powershell -command "Remove-Item cbm-create.ps1"
 pause
